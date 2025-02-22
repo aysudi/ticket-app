ef54b9db677cb01dc0ad18996799264caf48ead4
Aysu <aysu@Noutbuk-Aysu.local>
2025-02-22 19:10:59 +0400

    search and sort in favorites page

---
+++ src/js/helpers/sortSearch.js
@@ -0,0 +1,25 @@
+import { renderFavorites } from "./renderFavorites.js";
+
+export function sortItems(option, arr) {
+  switch (option) {
+    case "Price: High to Low":
+      return [...arr].sort((x, y) => y.price - x.price);
+      break;
+    case "Price: Low to High":
+      return [...arr].sort((x, y) => x.price - y.price);
+      break;
+    default:
+      return arr;
+  }
+}
+
+export function searchItems(arr) {
+  const searchInp = document.querySelector("#search");
+  searchInp.addEventListener("keyup", (e) => {
+    const searchQuery = e.target.value.trim().toLowerCase();
+    const searchedValue = arr.filter((x) =>
+      x.name.trim().toLowerCase().includes(searchQuery)
+    );
+    renderFavorites(searchedValue);
+  });
+}
