a26afc9382354344d9b3f3a6a46bc3283c3d4409
Aysu <aysu@Noutbuk-Aysu.local>
2025-02-23 19:44:42 +0400

    feat: register page submit done

---
+++ src/js/classes/User.js
@@ -0,0 +1,17 @@
+import moment from "moment";
+
+export class User {
+  constructor(fullName, username, email, password) {
+    this.role = "client";
+    this.username = username;
+    this.fullName = fullName;
+    this.email = email;
+    this.password = password;
+    this.balance = 0;
+    this.profilePictureURL =
+      "https://cdn-icons-png.freepik.com/512/13126/13126995.png";
+    this.favorites = [];
+    this.accountCreationDate = moment().format("L");
+    this.totalSpentMoney = 0;
+  }
+}
